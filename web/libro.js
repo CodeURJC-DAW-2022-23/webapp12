0713716b097d2a7550cdd6da0b5f2d1aa8824620
pamonugi <pamonugi@gmail.com>
2023-02-11 20:30:19 +0100

    Estrellitas :)

--- web/libro.js
+++ web/libro.js
@@ -1,12 +1,15 @@
-$(function () {
- 
-    $("#rateYo").rateYo({
-      rating: 3.6
-    });
-   
+$(document).ready(function(){
+  $("#rateYo1").rateYo({
+    rating: $("#nValoracion1").html()
   });
 
+  $("#rateYo2").rateYo({
+    rating: $("#nValoracion2").html()
+  });
 
-$("#rateYo1").rateYo({
-    rating: $("#nValoracion1").html()
+  $("#rateYo3").rateYo({
+    rating: $("#nValoracion3").html()
+  });
+
+  $('.nValoracion').hide();
 });
