c770f85d7cdc94708405f2e329ff4ca75a304751
pamonugi <pamonugi@gmail.com>
2023-02-13 11:31:53 +0100

    Estrellitas en el inicio

---
+++ web/Inicio.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    
+    $("#rateYo1").rateYo({
+      rating: $("#nValoracion1").html()
+    });
+  
+    $("#rateYo2").rateYo({
+      rating: $("#nValoracion2").html()
+    });
+  
+    $("#rateYo3").rateYo({
+      rating: $("#nValoracion3").html()
+    });
+
+    $("#rateYo4").rateYo({
+        rating: $("#nValoracion4").html()
+    });
+  
+    $("#rateYo5").rateYo({
+        rating: $("#nValoracion5").html()
+    });
+
+    $("#rateYo6").rateYo({
+        rating: $("#nValoracion6").html()
+    });
+
+    $("#rateYo7").rateYo({
+        rating: $("#nValoracion7").html()
+    });
+
+    $("#rateYo8").rateYo({
+        rating: $("#nValoracion8").html()
+    });
+
+    $("#rateYo9").rateYo({
+        rating: $("#nValoracion9").html()
+    });
+
+    $("#rateYo10").rateYo({
+        rating: $("#nValoracion10").html()
+    });
+
+    $("#rateYo11").rateYo({
+        rating: $("#nValoracion11").html()
+    });
+
+    $("#rateYo12").rateYo({
+        rating: $("#nValoracion12").html()
+    });
+
+    $('.nValoracion').hide();
+  });
+  
