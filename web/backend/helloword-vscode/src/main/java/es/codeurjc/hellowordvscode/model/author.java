2eb07b3bd1495c6cef3f016469ef0b500c326835
pamonugi <pamonugi@gmail.com>
2023-02-21 17:02:44 +0100

    Book and author

--- web/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/model/author.java
+++ web/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/model/author.java
@@ -1,5 +1,53 @@
 package es.codeurjc.hellowordvscode.model;
+import java.sql.Blob;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="authors")
 public class author {
-    
+
+	@Id
+	private String name;
+
+	private Blob portrait;
+    private String description;
+
+
+	public author() {
+	}
+
+	public author(String name, String description, Blob portrait) {
+		this.name = name;
+		this.description = description;
+		this.portrait = portrait;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	 
+    public Blob getPortrait(){
+        return portrait;
+    }
+
+    public void setPortrait(Blob portrait){
+        this.portrait = portrait;
+    }
 }
