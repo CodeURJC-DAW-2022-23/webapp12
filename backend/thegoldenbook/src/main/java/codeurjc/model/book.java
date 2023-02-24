13cf026738b29a648afc1c0e7847095af9304585
lailaelkhattabielhas <laiaek405gmail.com>
2023-02-24 20:20:01 +0100

    carpetamodel con todo lo de java

---
+++ backend/thegoldenbook/src/main/java/codeurjc/model/book.java
@@ -0,0 +1,83 @@
+package codeurjc.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table (name = "bookTable")
+public class book  implements Serializable{
+
+    @EmbeddedId
+    private bookTitleAuthor BookTitleAuthor;
+
+    @Column
+    private int stock;
+
+    @Column
+    private int price;
+
+    @Column
+    private String synopsis;
+
+    @ManyToOne
+    private author Author;
+    
+    @OneToMany (mappedBy = "Book")
+    private List<review> Reviews;
+
+    @ManyToMany
+    private List<shell> Shell;
+
+    public book() {
+    }
+
+    public book(bookTitleAuthor bookTitleAuthor, int stock, int price, String synopsis) {
+        BookTitleAuthor = bookTitleAuthor;
+        this.stock = stock;
+        this.price = price;
+        this.synopsis = synopsis;
+    }
+
+    public bookTitleAuthor getBookTitleAuthor() {
+        return BookTitleAuthor;
+    }
+
+    public void setBookTitleAuthor(bookTitleAuthor bookTitleAuthor) {
+        BookTitleAuthor = bookTitleAuthor;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+}
+
---
+++ backend/thegoldenbook/src/main/java/codeurjc/model/review.java
@@ -0,0 +1,66 @@
+package codeurjc.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity 
+@Table(name = "ReviewTable")
+public class review implements Serializable  {
+
+
+    @EmbeddedId
+    private reviewUserBook ReviewUserBook;
+
+    @Column
+    private int  calification;
+
+    @Column
+    private String Text;
+
+    @ManyToOne
+    private user User;
+
+    @ManyToOne
+    private book Book;
+
+    public review() {
+    }
+
+    public review(reviewUserBook reviewUserBook, int calification, String text) {
+        ReviewUserBook = reviewUserBook;
+        this.calification = calification;
+        Text = text;
+    }
+
+    public reviewUserBook getReviewUserBook() {
+        return ReviewUserBook;
+    }
+
+    public void setReviewUserBook(reviewUserBook reviewUserBook) {
+        ReviewUserBook = reviewUserBook;
+    }
+
+    public int getCalification() {
+        return calification;
+    }
+
+    public void setCalification(int calification) {
+        this.calification = calification;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+
+    
+    
+}
