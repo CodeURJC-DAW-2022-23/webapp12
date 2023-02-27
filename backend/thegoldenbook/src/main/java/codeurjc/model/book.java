e8e2aa5d2173ad60068abd72d5cfc1254eb1661c
XdeXavi <medinaxavi.22@gmail.com>
2023-02-27 18:01:41 +0100

    book v2

--- backend/thegoldenbook/src/main/java/codeurjc/model/book.java
+++ backend/thegoldenbook/src/main/java/codeurjc/model/book.java
@@ -1,16 +1,10 @@
 package codeurjc.model;
 
-import java.io.Serializable;
 import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
