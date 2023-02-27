2665f6ee896a69070247883ce3d569aa89597815
DiosYisus77 <jesusp.sanchez77@gmail.com>
2023-02-27 20:40:15 +0100

    authorController v2

--- backend/thegoldenbook/src/main/java/codeurjc/controller/AuthorController.java
+++ backend/thegoldenbook/src/main/java/codeurjc/controller/AuthorController.java
@@ -1,10 +1,14 @@
 package codeurjc.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import codeurjc.model.Author;
 import codeurjc.repository.AuthorRepository;
 
 @Controller
@@ -13,14 +17,20 @@ public class AuthorController{
     @Autowired
     private AuthorRepository author_repository;
 
-    @GetMapping("/modifyAuthor")
-    public String modifyAuthor(Model model){
-        return "modifyAuthor";
-    }
     @GetMapping("/author")
     public String author(Model model){
-        model.addAttribute("name","Stephen King");
-        model.addAttribute("info", "text");
+        author_repository.save(new Author("Stephen", "King"));
+        author_repository.save(new Author("Laura", "Gallego"));
+        model.addAttribute("authorList", author_repository.findAll());
+        return "authorList";
+    }
+
+    @GetMapping("/author/{id}")
+    public String showAuthor(Model model, @PathVariable long id){
+        Optional<Author> author = author_repository.findById(id);
+        if (author.isPresent()){
+            model.addAttribute("author", author.get());
+        }
         return "author";
     }
 }
