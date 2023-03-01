8f0a1125f68cb8146a21c3cee15d0baf7a4268a0
DiosYisus77 <jesusp.sanchez77@gmail.com>
2023-03-01 11:32:54 +0100

    User Model FINAL

--- backend/thegoldenbook/src/main/java/codeurjc/model/User.java
+++ backend/thegoldenbook/src/main/java/codeurjc/model/User.java
@@ -1,42 +1,50 @@
 package codeurjc.model;
+import java.util.List;
 
-
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity 
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     private String name;
     private String email;
-    private String adress;
-    private int phoneNumber;
     private String password;
-    private String favoriteGenre;
+    private String phoneNumber;
+    private String address;    
 
+    @OneToMany
+    private List<Review> reviewList;
     
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
 
-   // @OneToMany(mappedBy = "User")
-    //private List<Review> reviews;
-
-    public User() {
-    }
-
+    public User() {}
 
     public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
-        this.adress = "";
-        this.phoneNumber = 0;
         this.password = password;
-        this.favoriteGenre = "";
+        this.address = "null";
+        this.phoneNumber = "00";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -51,41 +59,9 @@ public class User {
         return email;
     }
 
-
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-
-    public String getAdress() {
-        return adress;
-    }
-
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-
-    
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-
-    public String getFavoriteGenre() {
-        return favoriteGenre;
-    }
-
-    public void setFavoriteGenre(String favoriteGenre) {
-        this.favoriteGenre = favoriteGenre;
-    }
-    
 
     public String getPassword() {
         return password;
@@ -95,5 +71,27 @@ public class User {
         this.password = password;
     }
 
-    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    } 
 }
