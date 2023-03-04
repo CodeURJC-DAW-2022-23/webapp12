2281b888f69dc7121e26b3d197784e44ea7c69b5
lailaelkhattabielhas <laiaek405gmail.com>
2023-03-04 18:54:20 +0100

    aniadir autr sin imagen

--- backend/thegoldenbook/src/main/java/codeurjc/controller/AuthorController.java
+++ backend/thegoldenbook/src/main/java/codeurjc/controller/AuthorController.java
@@ -1,5 +1,6 @@
 package codeurjc.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import codeurjc.model.Author;
 import codeurjc.repository.AuthorRepository;
+
 
 @Controller
 public class AuthorController{
@@ -38,13 +40,21 @@ public class AuthorController{
     }
 
     @GetMapping("/authorModification")
-    public String authorModification(){
-        return "/authorModification";
+    public String authorModification(Model model){
+        return "authorModification";
     }
 
-    @RequestMapping("/authorModification")
-    public String addAuthor(@RequestParam String fname, @RequestParam String lname, @RequestParam String info){ //hay que añadir la imagen
-        author_repository.save(new Author(fname, lname, info));
-        return "redirect:/author";
+
+    @PostMapping("/authorModification")
+    public String newAuthorModification(Model model, Author author) throws IOException{
+
+        
+
+
+        author_repository.save(author);
+
+        return "redirect:/author/";
+
     }
+
 }
