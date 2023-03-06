e617834a406276fc694c24192869a2c34aa0d740
XdeXavi <medinaxavi.22@gmail.com>
2023-03-06 12:28:36 +0100

    cambios en CartController

---
+++ backend/thegoldenbook/src/main/java/codeurjc/model/Cart.java
@@ -0,0 +1,51 @@
+package codeurjc.model;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Cart{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private int amounthTotal;
+    private double total; 
+
+    public Cart() {}
+
+    public Cart(int amounthTotal, double total){
+        this.amounthTotal = amounthTotal;
+        this.total = total;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAmounthTotal() {
+        return amounthTotal;
+    }
+
+    public void setAmounthTotal(int amounthTotal) {
+        this.amounthTotal = amounthTotal;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+}
