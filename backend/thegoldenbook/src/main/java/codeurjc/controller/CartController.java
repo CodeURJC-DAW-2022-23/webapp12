88ec1cf9fb4c625a688a66dd0c19fd448975c782
XdeXavi <medinaxavi.22@gmail.com>
2023-03-09 17:58:48 +0100

    1 version de cartController

--- backend/thegoldenbook/src/main/java/codeurjc/controller/CartController.java
+++ backend/thegoldenbook/src/main/java/codeurjc/controller/CartController.java
@@ -1,10 +1,16 @@
 package codeurjc.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import codeurjc.model.Book;
 import codeurjc.repository.CartRepository;
 
 @Controller
@@ -18,5 +24,14 @@ public class CartController {
         model.addAttribute("bookList", cart_repository.findAll());
     return "cart";
     }
+
+    @GetMapping("/bookCart/{id}")
+    public String bookInfo(Model model, @PathVariable long id, HttpServletRequest request){
+        Optional<Book> book = book_repository.findById(id);
+          if (book.isPresent()){
+              model.addAttribute("book", book.get());
+          }
+          return "cart";
+      }
 
 }
