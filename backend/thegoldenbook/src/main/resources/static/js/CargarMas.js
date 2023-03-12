caff10a9730e56cb5612b4db765bd9cd50426080
Gabriel15010 <103439723+Gabriel15010@users.noreply.github.com>
2023-03-12 13:55:27 +0100

    Caargar mas v2

--- backend/thegoldenbook/src/main/resources/static/js/CargarMas.js
+++ backend/thegoldenbook/src/main/resources/static/js/CargarMas.js
@@ -1,48 +1,43 @@
-var currentPage = 0;
-const valores = window.location.search;
+// Obtener el botón de "Cargar más libros"
+const btnCargarMasLibros = document.getElementById("btn-cargar-mas-libros");
 
-function moreContent() {
-    switchMoreContentButtonActivation(true)
-    currentPage = currentPage + 1;
-    console.log(valores);
-    const urlParams = new URLSearchParams(valores);
-    var searchtext = urlParams.get('searchtext');
-    if (searchtext == null)
-        $.ajax({
-            type: 'GET',
-            url: '/books?currentPage=' + currentPage,
-            error: function () {
-                console.log('Something went wrong')
-                switchMoreContentButtonActivation(false)
-            },
-            success: function (data) {
-                var books = $(data).find('#Books').html();
-                var noMoreBooks = $(data).find('#noMoreBooks').html();
-                if (noMoreBooks == undefined) {
-                    $('#Books').append(books)
-                    switchMoreContentButtonActivation(false)
-                } else {
-                    $('#moreContentButton').css("display", "none");
-                }
-            }
-        }) 
-    else
-        $.ajax({
-            type: 'GET',
-            url: '/books?searchtext=' + searchtext + '&currentPage=' + currentPage,
-            error: function () {
-                console.log('Something went wrong')
-                switchMoreContentButtonActivation(false)
-            },
-            success: function (data) {
-                var books = $(data).find('#Books').html();
-                var noMoreBooks = $(data).find('#noMoreBooks').html();
-                if (noMoreBooks == undefined) {
-                    $('#Books').append(books)
-                    switchMoreContentButtonActivation(false)
-                } else {
-                    $('#moreContentButton').css("display", "none");
-                }
-            }
-        })
+// Añadir un evento click al botón
+btnCargarMasLibros.addEventListener("click", cargarMasLibros);
+
+// Función para cargar más libros
+function cargarMasLibros() {
+  // Obtener el contenedor de libros
+  const contenedorLibros = document.getElementById("contenedor-libros");
+
+  // Crear un objeto XMLHttpRequest
+  const xhr = new XMLHttpRequest();
+
+  // Establecer la URL del archivo que contiene los libros adicionales
+  const url = "libros-adicionales.html";
+
+  // Abrir una conexión con el servidor
+  xhr.open("GET", url, true);
+
+  // Establecer el tipo de respuesta que se espera
+  xhr.responseType = "document";
+
+  // Añadir un evento load para manejar la respuesta
+  xhr.onload = function() {
+    // Obtener la respuesta del servidor
+    const respuesta = xhr.response;
+
+    // Obtener los nuevos libros
+    const nuevosLibros = respuesta.querySelectorAll(".libro");
+
+    // Añadir los nuevos libros al contenedor
+    nuevosLibros.forEach(function(libro) {
+      contenedorLibros.appendChild(libro);
+    });
+  };
+
+  // Enviar la petición al servidor
+  xhr.send();
+
+  // Ocultar el botón de "Cargar más libros"
+  btnCargarMasLibros.style.display = "none";
 }
